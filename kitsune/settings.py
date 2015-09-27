349557e6a1c5f312b43c92f8d542e7f3439b7cd1
Thomas <silentbob73@users.noreply.github.com>
2015-09-27 07:38:25 +0200

    Update settings.py

--- kitsune/settings.py
+++ kitsune/settings.py
@@ -56,7 +56,7 @@ DATABASES = {
 DATABASE_ROUTERS = ('multidb.PinningMasterSlaveRouter',)
 
 # Put the aliases for your slave databases in this list
-SLAVE_DATABASES = []
+SLAVE_DATABASES = [true]
 
 # Cache Settings
 # CACHES = {
